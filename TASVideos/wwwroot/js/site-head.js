6083e8f3b99a0b4c86ed374de77de1e27f7053dc
TiKevin83 <travismcgeehan@gmail.com>
2022-03-31 21:05:09 -0400

    april 1

--- TASVideos/wwwroot/js/site-head.js
+++ TASVideos/wwwroot/js/site-head.js
@@ -55,3 +55,76 @@ if (localStorage.getItem("style-dark") !== null) {
 		forceDarkMode();
 	}
 }
+
+let displayRefreshRate = 60;
+
+/**
+ * Allows to obtain the estimated Hz of the primary monitor in the system.
+ * 
+ * @param {Function} callback The function triggered after obtaining the estimated Hz of the monitor.
+ * @param {Boolean} runIndefinitely If set to true, the callback will be triggered indefinitely (for live counter).
+ */
+function getScreenRefreshRate(callback, runIndefinitely) {
+    let requestId = null;
+    let callbackTriggered = false;
+    runIndefinitely = runIndefinitely || false;
+
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame;
+    }
+
+    let DOMHighResTimeStampCollection = [];
+
+    let triggerAnimation = function (DOMHighResTimeStamp) {
+        DOMHighResTimeStampCollection.unshift(DOMHighResTimeStamp);
+
+        if (DOMHighResTimeStampCollection.length > 10) {
+            let t0 = DOMHighResTimeStampCollection.pop();
+            let fps = Math.floor(1000 * 10 / (DOMHighResTimeStamp - t0));
+
+            if (!callbackTriggered) {
+                callback.call(undefined, fps, DOMHighResTimeStampCollection);
+            }
+
+            if (runIndefinitely) {
+                callbackTriggered = false;
+            } else {
+                callbackTriggered = true;
+            }
+        }
+
+        requestId = window.requestAnimationFrame(triggerAnimation);
+    };
+
+    window.requestAnimationFrame(triggerAnimation);
+
+    // Stop after half second if it shouldn't run indefinitely
+    if (!runIndefinitely) {
+        window.setTimeout(function () {
+            window.cancelAnimationFrame(requestId);
+            requestId = null;
+        }, 500);
+    }
+}
+
+function observer_callback(list, observer) {
+	// After 500ms, will output the estimated Hz of the monitor (frames per second - FPS)
+	// 239 FPS (in my case)
+	getScreenRefreshRate(function (FPS) {
+		displayRefreshRate = FPS;
+		const paintTimings = performance.getEntriesByType('paint');
+		const fmp = paintTimings.find(({ name }) => name === "first-contentful-paint");
+
+		const message = `Loading this page cost you ${Math.ceil(fmp.startTime / 1000 * displayRefreshRate)} frames`;
+
+		const node = document.createElement("div");
+		const textnode = document.createTextNode(message);
+		node.appendChild(textnode);
+		node.style = "color: RGBA(255,255,255,.6)";
+
+		document.getElementById("DarkDropdown").appendChild(node);
+	});
+}
+
+let observer = new PerformanceObserver(observer_callback);
+observer.observe({ entryTypes: ["paint"] });
